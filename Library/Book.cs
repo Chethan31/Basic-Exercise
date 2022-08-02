5aecd6b9ee4fc2e1873d24af49774a6f4fca7f69
Chethan Gowda <chethanrgowda38@gmail.com>
2022-08-02 17:24:45 +0530

    Add

---
+++ Library/Book.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class Book
+    {
+        private string title; 
+        private string author; 
+        private bool available; 
+ 
+        public Book(string title, string author, bool available)
+        { 
+            this.title = title;
+            this.author = author;   
+            this.available = available;
+            Console.WriteLine(this.title);
+            Console.WriteLine(this.author);
+            Console.WriteLine(this.available);
+        }
+        public Book(string title, string author):this(title, author, true)
+        {
+        }
+        public Book(string title)
+        {
+            this.title = title;
+            Console.WriteLine(this.title);
+        }
+        public Book()
+        {
+            title = "untitled";
+            author = "anonymous";
+            available = true;
+            Console.WriteLine(title);
+            Console.WriteLine(author);
+            Console.WriteLine(available);
+        }
+    }
+}
---
+++ Library/BookTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class BookTest
+    {
+        public static void Main()
+        {
+            Book book1 = new Book("asa","asas",true);
+            Book book2 = new Book("dsdas", "dssfsd");
+            Book book3 = new Book("asa");
+            Book book4 = new Book();
+        }
+    }
+}
